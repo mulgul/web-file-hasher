5f67a186c2306ab0150eaaaa882ae9485c8e39a9
tarikgul <tariksnow37@gmail.com>
2023-11-18 00:14:34 -0500

    small server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express')
+const app = express();
+const port = 8080;
+
+app.use('/', express.static(path.join(__dirname, '../dist')));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
