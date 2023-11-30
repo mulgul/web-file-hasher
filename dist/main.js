cb6f3c9dd9cee3e2cc4ce10466861769254bdfce
Ryan Mullen <ryanfmullen@gmail.com>
2023-11-30 13:21:26 -0800

    fix: add file listing and update styling

--- dist/main.js
+++ dist/main.js
@@ -10,6 +10,30 @@ async function run() {
     addDropDown();
 }
 
+const convertFileSize = (bytes, si = false, dp = 1) => {
+	const thresh = si ? 1000 : 1024;
+
+	if (Math.abs(bytes) < thresh) {
+		return bytes + ' B';
+	}
+
+	const units = si
+		? ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+		: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+	let u = -1;
+	const r = 10 ** dp;
+
+	do {
+		bytes /= thresh;
+		++u;
+	} while (
+		Math.round(Math.abs(bytes) * r) / r >= thresh &&
+		u < units.length - 1
+	);
+
+	return bytes.toFixed(dp) + ' ' + units[u];
+};
+
 function addDropDown() {
     const hashingTypes = hash_types();
 
@@ -17,8 +41,8 @@ function addDropDown() {
 }
 
 function addDragAndDrop(f) {
-    let dropArea = document.getElementById('drop-area');
-    let fileElem = document.getElementById('file-elem');
+    let dropArea = document.getElementById('dropArea');
+    let fileElem = document.getElementById('fileElem');
 
     function preventDefaults (e) {
         e.preventDefault()
@@ -40,14 +64,14 @@ function addDragAndDrop(f) {
 
         for(const file of files) {
             f.push(file)
-            addFileToList(file.name)
+            addFileToList(file)
         }
     }
 
     fileElem.onchange = () => {
         for (const file of fileElem.files) {
             f.push(file);
-            addFileToList(file.name)
+            addFileToList(file)
         }
     }
 
@@ -66,12 +90,20 @@ function addDragAndDrop(f) {
     dropArea.addEventListener('drop', handleDrop, false)
 }
 
-function addFileToList(fileName) {
+function addFileToList(file) {
     const item = document.getElementById('listFiles');
     const newLI = document.createElement('div');
-    newLI.setAttribute('id', 'filename-div')
-    newLI.textContent = fileName;
+    newLI.setAttribute('id', 'filename-div');
+    // const fileNameContainer = document.createElement('div');
+    // fileNameContainer.setAttribute('id', 'fileNameContainer');
+    const fileName = document.createElement('p');
+    fileName.textContent = file.name;
+    const fileSize = document.createElement('p');
+    fileSize.textContent = convertFileSize(file.size, true);
     item.appendChild(newLI);
+    // fileNameContainer.appendChild(fileName);
+    newLI.appendChild(fileName);
+    newLI.appendChild(fileSize);
 }
 
 run();
